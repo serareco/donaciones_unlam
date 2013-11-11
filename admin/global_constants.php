73b6fa004b2fed57cd17c7eb7ae3c455a7694843
Sebastian M. Perez <perezsebastianm@gmail.com>
2013-11-11 02:22:27 -0300

    Agrego el File System

---
+++ admin/global_constants.php
@@ -0,0 +1,11 @@
+<?php
+define('ROOT_FOLDER_NAME', 'donaciones');
+
+define('BASE_PATH', 'C:/xampp/htdocs/'. ROOT_FOLDER_NAME .'/admin');
+define('SOURCE_PATH', '/'.ROOT_FOLDER_NAME.'/source/');
+
+//APPLICATION CONSTANTS
+define('APP_NAME','Proyecto UNLAM');	//NOMBRE DE LA APLICACION
+define('APP_DEVELOPER','spor3');	//EMPRESA DESSARROLLADORA
+define('APP_COMPANY','UNLAM');	//CLIENTE
+define('APP_TITLE','Proyecto UNLAM :: Sistema de Administración');	//TITULO DE LA APLICACION
